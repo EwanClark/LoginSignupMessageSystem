1841c6420acace5a873b99bba8150c2c4ef604f8
Bubllz <ewancclark@outlook.com>
2024-09-14 20:46:43 +0100

    remove python files for checking how the api works

--- accses the delete shorturl.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-response = requests.post("https://api.bubllz.com/removeshorturl", json={"shorturl": ""}, headers={"token": "83fe2c76a1c4ba162542a978ed02275bd2671f103e39dfff238739b25e0c0e3df29722b22885ec7f66a1bd446f3d042d73badd18eaf951e4b2f1fd091157952f"})
-
-print(response.text)
-print(response.status_code)
